82fabf4438ca9c1974fcfce2bfe3e236d4c5cd07
mehrzadian <amirreza.313.mehrzadian@gmail.com>
2023-04-28 12:16:57 +0430

    implement function for sequence of people

--- code/knights.py
+++ code/knights.py
@@ -17,3 +17,29 @@ class Person:
     def __str__(self):
         return str(self.honesty)+"-"+str(self.id)
         
+import random
+     
+n = int(input())
+def seq(n):
+    '''create the sequence of people with respect to n 
+    in which  honests are more'''
+    ls=[]
+    zeros=0
+    for i in range(n):
+        r = random.randint(0,1)
+        if r==0:
+            zeros+=1
+            if n%2==1 and zeros<= n//2:
+                p=Person(honesty=False,id=i)
+            elif n%2==0 and zeros< n//2:
+                p=Person(honesty=False,id=i)
+            else:
+                p=Person(honesty=True,id=i)
+        else:
+            p=Person(True,id=i)
+        ls.append(p)
+    return ls
+
+ls=seq(n)
+for i in range(n):
+    print(ls[i])
